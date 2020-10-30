3cc8f7970c84343a1acd9ab651378892cf2b92f5
jaysmit8340 <jasonsmit95@gmail.com>
2020-10-30 13:21:25 -0700

    Added code to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+    var form = document.getElementsByTagName('form').item(0);
+    form.onsubmit = function(){
+        return addEmail(this);
+    };
+}
+
+function addEmail(formData){
+    let newEmail = formData.email.value;
+    var messageElement = document.querySelector('.message');
+    var message = null;
+    if(newEmail){
+        message =`Thank you! Your email address ${newEmail} has been added to your mailing list`;
+    }else{
+         message = "Please enter a validate email";
+    }
+    messageElement.innerHTML = message;
+    return false
+
+}
